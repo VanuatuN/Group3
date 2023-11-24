04da5751bdfa537226ca7629c5539d33bfffa171
jalmerol <jalmerol@ictp.it>
2023-11-24 16:45:45 +0100

    Modified to add cpp compatibility

--- include/compute_force.h
+++ include/compute_force.h
@@ -1,11 +1,30 @@
-#ifndef COMPUTE_FORCE_H
-#define COMPUTE_FORCE_H
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
+// #ifndef C_COMPUTE_FORCE_H
+// #define C_COMPUTE_FORCE_H
+// #include <stdio.h>
+// #include <string.h>
+// #include <math.h>
+// #include "datastructure.h"
+// #include "utilities.h"
+
+// extern "C" {
+// void force(mdsys_t *sys);
+// }
+
+// #endif
+
+
+// // compute_force.h
+#ifndef C_COMPUTE_FORCE_H
+#define C_COMPUTE_FORCE_H
 #include "datastructure.h"
-#include "utilities.h"
 
+/* compute forces */
+#ifdef __cplusplus
+extern "C" {
 void force(mdsys_t *sys);
-
+} 
+#else 
+void force(mdsys_t *sys);
 #endif
+
+#endif /* COMPUTE_FORCE_H */
