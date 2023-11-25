0d894a72d776f1570c15fdce59ad585b9e5ae054
jalmerol <jalmerol@ictp.it>
2023-11-25 14:34:35 +0100

    Added clean and working serial code for ljmd

---
+++ include/compute_force.h
@@ -0,0 +1,16 @@
+// compute_force.h
+#ifndef C_COMPUTE_FORCE_H
+#define C_COMPUTE_FORCE_H
+#include "datastructure.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void force(mdsys_t *sys);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* COMPUTE_FORCE_H */
