fca5239a01e13a0702229cf7cfb308282c998cc3
jalmerol <jalmerol@ictp.it>
2023-11-26 22:36:40 +0100

    Added more tests for particles within rcut with PBC

--- tests/test_compute_force.cpp
+++ tests/test_compute_force.cpp
@@ -35,6 +35,38 @@ TEST(ComputeForce, TwoParticleInsideCutoff) {
     EXPECT_DOUBLE_EQ(sys.fz[0], 0.0);
 }
 
+TEST(ComputeForce, TwoParticleInsideCutoffNearCuttoff) {
+    mdsys_t sys;
+    init_mdsys(&sys);
+    sys.natoms = 2;
+    sys.mass = 39.948;
+    sys.epsilon = 0.2379;
+    sys.sigma = 3.405;
+    sys.rcut = 8.5; // Set your cutoff distance
+    sys.box = 17.1580;
+
+    sys.epot=0.0;
+    azzero(sys.fx,sys.natoms);
+    azzero(sys.fy,sys.natoms);
+    azzero(sys.fz,sys.natoms);
+
+    // Set positions of particles inside cutoff
+    sys.rx[0] = 0.0;
+    sys.ry[0] = 0.0;
+    sys.rz[0] = 0.0;
+
+    sys.rx[1] = 0.0;
+    sys.ry[1] = 8.45;
+    sys.rz[1] = 0.0;
+
+    force(&sys);
+
+    // Add assertions to check the forces
+    EXPECT_DOUBLE_EQ(sys.fx[0], 0.0);
+    EXPECT_DOUBLE_EQ(sys.fy[0], 0.0028679847024252691);
+    EXPECT_DOUBLE_EQ(sys.fz[0], 0.0);
+}
+
 TEST(ComputeForce, TwoParticleOutsideCutoff) {
     mdsys_t sys;
     init_mdsys(&sys);
@@ -65,4 +97,68 @@ TEST(ComputeForce, TwoParticleOutsideCutoff) {
     EXPECT_DOUBLE_EQ(sys.fx[0], 0.0);
     EXPECT_DOUBLE_EQ(sys.fy[0], 0.0);
     EXPECT_DOUBLE_EQ(sys.fz[0], 0.0);
+}
+
+TEST(ComputeForce, TwoParticleInsideCutoffPBC) {
+    mdsys_t sys;
+    init_mdsys(&sys);
+    sys.natoms = 2;
+    sys.mass = 39.948;
+    sys.epsilon = 0.2379;
+    sys.sigma = 3.405;
+    sys.rcut = 8.5; // Set your cutoff distance
+    sys.box = 17.1580;
+
+    sys.epot=0.0;
+    azzero(sys.fx,sys.natoms);
+    azzero(sys.fy,sys.natoms);
+    azzero(sys.fz,sys.natoms);
+
+    // Set positions of particles inside cutoff
+    sys.rx[0] = 0.0;
+    sys.ry[0] = 0.0;
+    sys.rz[0] = 0.0;
+
+    sys.rx[1] = 17.0;
+    sys.ry[1] = 0.0;
+    sys.rz[1] = 0.0;
+
+    force(&sys);
+
+    // Add assertions to check the forces
+    EXPECT_DOUBLE_EQ(sys.fx[0], 7.2526871094147392e+17);
+    EXPECT_DOUBLE_EQ(sys.fy[0], 0.0);
+    EXPECT_DOUBLE_EQ(sys.fz[0], 0.0);
+}
+
+TEST(ComputeForce, TwoParticleInsideCutoffPBC1) {
+    mdsys_t sys;
+    init_mdsys(&sys);
+    sys.natoms = 2;
+    sys.mass = 39.948;
+    sys.epsilon = 0.2379;
+    sys.sigma = 3.405;
+    sys.rcut = 8.5; // Set your cutoff distance
+    sys.box = 17.1580;
+
+    sys.epot=0.0;
+    azzero(sys.fx,sys.natoms);
+    azzero(sys.fy,sys.natoms);
+    azzero(sys.fz,sys.natoms);
+
+    // Set positions of particles inside cutoff
+    sys.rx[0] = 0.0;
+    sys.ry[0] = 0.0;
+    sys.rz[0] = 0.0;
+
+    sys.rx[1] = 0.0;
+    sys.ry[1] = 0.0;
+    sys.rz[1] = 11.0;
+
+    force(&sys);
+
+    // Add assertions to check the forces
+    EXPECT_DOUBLE_EQ(sys.fx[0], 0.0);
+    EXPECT_DOUBLE_EQ(sys.fy[0], 0.0);
+    EXPECT_DOUBLE_EQ(sys.fz[0], -0.024984372427535984);
 }
