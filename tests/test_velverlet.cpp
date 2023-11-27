28d640c50a4374dedf83dea9c352726c43080321
jalmerol <jalmerol@ictp.it>
2023-11-27 11:06:13 +0100

    Fixed bug in test_velverlet

--- tests/test_velverlet.cpp
+++ tests/test_velverlet.cpp
@@ -12,11 +12,31 @@ TEST(VelocityVerletTest, UpdateHalf1) {
     // Test update_half1 function
     mdsys_t sys;
     init_mdsys(&sys);
-   
+
+    sys.natoms = 2;
+    sys.mass = 39.948;
+    sys.epsilon = 0.2379;
+    sys.sigma = 3.405;
+    sys.rcut = 8.5; // Set your cutoff distance
+    sys.box = 17.1580;
+
     sys.dt = 5.0;
     int i = 0; // or any other valid index
 
     // Save initial state for comparison
+
+    sys.rx[i] = 0.0;
+    sys.ry[i] = 0.0;
+    sys.rz[i] = 0.0;
+
+    sys.vx[i] = 0.1;
+    sys.vy[i] = 0.1;
+    sys.vz[i] = 0.1;
+
+    sys.fx[i] = 0.1;
+    sys.fy[i] = 0.1;
+    sys.fz[i] = 0.1;
+
     double initial_rx = sys.rx[i];
     double initial_ry = sys.ry[i];
     double initial_rz = sys.rz[i];
@@ -24,7 +44,6 @@ TEST(VelocityVerletTest, UpdateHalf1) {
     double initial_vy = sys.vy[i];
     double initial_vz = sys.vz[i];
 
-   
     // Call the function
     update_half1(&sys, i);
 
