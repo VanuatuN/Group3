5d8e3436c8e6a2fd75727eb518178e9adf3c2ae3
ed-ntim <egasu24@gmail.com>
2023-11-27 19:42:10 +0100

    added refactoring to the force kernel

--- src/compute_force.c
+++ src/compute_force.c
@@ -47,64 +47,14 @@ void force(mdsys_t *sys)
 
 #else
 
-// void force(mdsys_t *sys)
-// {
-//     double r, ffac, inv_r, inv_r6, inv_r12;
-//     double rx, ry, rz;
-//     int i, j;
-
-//     /* zero energy and forces */
-//     sys->epot = 0.0;
-//     azzero(sys->fx, sys->natoms);
-//     azzero(sys->fy, sys->natoms);
-//     azzero(sys->fz, sys->natoms);
-
-//     for (i = 0; i < (sys->natoms); ++i)
-//     {
-//         for (j = i + 1; j < (sys->natoms); ++j)
-//         {
-//             /* get distance between particle i and j */
-//             rx = pbc(sys->rx[i] - sys->rx[j], 0.5 * sys->box);
-//             ry = pbc(sys->ry[i] - sys->ry[j], 0.5 * sys->box);
-//             rz = pbc(sys->rz[i] - sys->rz[j], 0.5 * sys->box);
-
-//             r = sqrt(rx * rx + ry * ry + rz * rz);
-//             inv_r = 1.0 / r;
-
-//             /* compute force and energy if within cutoff */
-//             if (r < sys->rcut)
-//             {
-//                 inv_r6 = inv_r * inv_r * inv_r * inv_r * inv_r * inv_r;
-//                 inv_r12 = inv_r6 * inv_r6;
-
-//                 ffac = 4.0 * sys->epsilon * (12.0 * inv_r12 - 6.0 * inv_r6);
-
-//                 sys->epot += 0.5 * 4.0 * sys->epsilon * (inv_r12 - inv_r6);
-
-//                 // Update forces using vectorized operations
-//                 double force_i = rx * inv_r * ffac;
-//                 double force_j = -rx * inv_r * ffac;
-
-//                 sys->fx[i] += force_i;
-//                 sys->fy[i] += ry * inv_r * ffac;
-//                 sys->fz[i] += rz * inv_r * ffac;
-
-//                 sys->fx[j] += force_j;
-//                 sys->fy[j] -= ry * inv_r * ffac;
-//                 sys->fz[j] -= rz * inv_r * ffac;
-//             }
-//         }
-//     }
-// }
-// #endif 
-
 
 /* Optimized force computation */
 void force(mdsys_t *sys)
 {
-    double r, ffac, inv_r, inv_r6, inv_r12;
-    double rx, ry, rz;
-    int i, j;
+    double c12 = 4.0 * sys->epsilon * pow(sys->sigma, 12.0);
+    double c6 = 4.0 * sys->epsilon * pow(sys->sigma, 6.0);
+    double rcsq = sys->rcut * sys->rcut;
+    double rx, ry, rz, rsq, ffac;
 
     /* zero energy and forces */
     sys->epot = 0.0;
@@ -112,36 +62,34 @@ void force(mdsys_t *sys)
     azzero(sys->fy, sys->natoms);
     azzero(sys->fz, sys->natoms);
 
-    for (i = 0; i < (sys->natoms); ++i)
+    for (int i = 0; i < (sys->natoms) - 1; ++i)
     {
-        for (j = i + 1; j < (sys->natoms); ++j)
+        for (int j = i + 1; j < (sys->natoms); ++j)
         {
-            /* get distance between particle i and j */
             rx = pbc(sys->rx[i] - sys->rx[j], 0.5 * sys->box);
             ry = pbc(sys->ry[i] - sys->ry[j], 0.5 * sys->box);
             rz = pbc(sys->rz[i] - sys->rz[j], 0.5 * sys->box);
+            rsq = rx * rx + ry * ry + rz * rz;
 
-            r = sqrt(rx * rx + ry * ry + rz * rz);
-            inv_r = 1.0 / r;
-            inv_r6 = inv_r * inv_r * inv_r * inv_r * inv_r * inv_r;
-            inv_r12 = inv_r6 * inv_r6;
-
-            /* compute force and energy if within cutoff */
-            if (r < sys->rcut)
+            if (rsq < rcsq)
             {
-                ffac = 4.0 * sys->epsilon * (12.0 * inv_r12 - 6.0 * inv_r6);
+                double r6, rinv;
+                rinv = 1.0 / rsq;
+                r6 = rinv * rinv * rinv;
+                ffac = (12.0 * c12 * r6 - 6.0 * c6) * r6 * rinv;
 
-                sys->epot += 0.5 * 4.0 * sys->epsilon * (inv_r12 - inv_r6);
+                sys->epot += r6 * (c12 * r6 - c6);
 
                 // Update forces using vectorized operations
-                sys->fx[i] += rx * inv_r * ffac;
-                sys->fy[i] += ry * inv_r * ffac;
-                sys->fz[i] += rz * inv_r * ffac;
+                sys->fx[i] += rx * ffac;
+                sys->fy[i] += ry * ffac;
+                sys->fz[i] += rz * ffac;
 
                 // Newton's third law: opposite force on particle j
-                sys->fx[j] -= rx * inv_r * ffac;
-                sys->fy[j] -= ry * inv_r * ffac;
-                sys->fz[j] -= rz * inv_r * ffac;
+                sys->fx[j] -= rx * ffac;
+                sys->fy[j] -= ry * ffac;
+                sys->fz[j] -= rz * ffac;
+                
             }
         }
     }
