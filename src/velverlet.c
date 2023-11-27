5f2b9795cdcc9abcdfb0b30521a23a5536570a67
Natalia Tilinina <natalia.tilinina@gmail.com>
2023-11-27 09:41:54 +0100

    test velv working

--- src/velverlet.c
+++ src/velverlet.c
@@ -25,9 +25,6 @@ void update_half2(mdsys_t *sys, int i)
         sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
         sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
         sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
-//         sys->rx[i] += sys->dt * sys->vx[i];
-//         sys->ry[i] += sys->dt * sys->vy[i];
-//         sys->rz[i] += sys->dt * sys->vz[i];
     }
 }
 
