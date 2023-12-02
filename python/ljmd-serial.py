1446859b123d0efca49f97f6fdd73494e617eb51
jalmerol <jalmerol@ictp.it>
2023-12-02 15:52:11 +0100

    Modified and currently working for the serial version of lennard-jones

--- python/ljmd-serial.py
+++ python/ljmd-serial.py
@@ -1,4 +1,5 @@
 from ctypes import *
+import time
 try:
     from mpi4py import MPI
     _MPI = True
@@ -6,11 +7,11 @@ except ImportError:
     _MPI = False
 
 mdlib = CDLL('../build-serial/libmd_lib.dylib')
-
 class MDSYS(Structure):
     _fields_ = (
-        ("natoms", c_int), ("nfi", c_int), ("nsteps", c_int),
+        ("natoms", c_int), ("nfi", c_int), ("nsteps", c_int), ("nthreads", c_int), ("thid", c_int),
         ("dt", c_double), ("mass", c_double), ("epsilon", c_double), ("sigma", c_double), ("box", c_double), ("rcut", c_double),
+        ("ekin", c_double), ("epot", c_double), ("temp", c_double),
         ("rx", POINTER(c_double)), ("ry", POINTER(c_double)), ("rz", POINTER(c_double)),
         ("vx", POINTER(c_double)), ("vy", POINTER(c_double)), ("vz", POINTER(c_double)),
         ("fx", POINTER(c_double)), ("fy", POINTER(c_double)), ("fz", POINTER(c_double))
@@ -32,7 +33,20 @@ def read_params(file_path):
                 params.append(words[0])
     return params
 
-def init_params(md, params, restart, xyz, data):
+def output(md, trajfile, ergfile):
+    print(f"{md.nfi:8d} {md.temp:20.8f} {md.ekin:20.8f} {md.epot:20.8f} {(md.ekin + md.epot):20.8f}")
+    
+    with open(ergfile[0], 'a') as file1:
+        file1.write(f"{md.nfi:8d} {md.temp:20.8f} {md.ekin:20.8f} {md.epot:20.8f} {(md.ekin + md.epot):20.8f}\n")
+    
+    with open(trajfile[0], 'a') as file2:
+        file2.write(f"{md.natoms:8d}\n nfi={md.nfi:8d} etot={md.ekin+md.epot:20.8f}\n")
+        
+        for i in range(md.natoms):
+            file2.write(f"{md.rx[i]:20.8f} {md.ry[i]:20.8f} {md.rz[i]:20.8f}\n")
+
+
+def init_params(md, params, restfile, trajfile, ergfile):
     md.natoms = int(params[0])
     md.mass = float(params[1])
     md.epsilon = float(params[2])
@@ -55,14 +69,11 @@ if __name__ == "__main__":
     restfile = [None]
     trajfile = [None]
     ergfile = [None]
-
-    if _MPI:
-        comm = MPI.COMM_WORLD
-        md.rank = comm.Get_rank()
-        md.npes = comm.Get_size()
+    md.nthreads = 1
+    md.thid = 0
 
     print("LJMD_VERSION ", LJMD_VERSION)
-    t_start = mdlib.wallclock()
+    t_start = time.time()
 
     params = read_params(file_path)
     init_params(md, params, restfile, trajfile, ergfile)
@@ -75,28 +86,18 @@ if __name__ == "__main__":
     mdlib.force(byref(md))
     mdlib.ekin(byref(md))
 
-    efile = ergfile[0].encode('utf-8')
-    tfile = trajfile[0].encode('utf-8')
-
-    erg = mdlib.openfile(efile, "w")
-    traj = mdlib.openfile(tfile, "w")
-
-    print("Startup time: ", mdlib.wallclock()-t_start)
-    print("Starting simulation with ", md.natoms, " atoms for ", md.nsteps, " steps")
+    print("Startup time: ", time.time()-t_start)
+    print("Starting simulation with ", md.natoms, " atoms for ", md.nsteps)
     print("     NFI            TEMP            EKIN                 EPOT              ETOT")
 
-    t_start = mdlib.wallclock()
-    for i in range(md.nfi + 1, md.nsteps):
-      md.nfi = i
-      if ((md.nfi % md.nprint)):
-          print(" i ", i)
-          mdlib.output(byref(md), erg, traj)
-      mdlib.velverlet(byref(md))
-      mdlib.ekin(byref(md))
+    t_start = time.time()
+    for i in range(md.nfi + 1, md.nsteps + 1, 1):
+        md.nfi = i
+        if ((md.nfi % md.nprint)==0):
+            output(md, trajfile, ergfile)
+        mdlib.velverlet(byref(md))
+        mdlib.ekin(byref(md))
 
-    print("Simulation Done. Run time: ", mdlib.wallclock()-t_start)
-
-    mdlib.closefile(erg)
-    mdlib.closefile(traj)
+    print("Simulation Done. Run time: ", time.time()-t_start)
 
     mdlib.cleanup_mdsys(byref(md))
