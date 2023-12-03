829aaa20e3ae5f2162aee892d9e910978b4b0504
jalmerol <jalmerol@ictp.it>
2023-12-03 23:02:47 +0100

    Modified parts with mpi for python main

--- include/utilities.h
+++ include/utilities.h
@@ -18,10 +18,12 @@ extern double wallclock();
 extern void azzero(double *d, const int n);
 extern double pbc(double x, const double boxby2);
 extern void ekin(mdsys_t *sys);
+#if defined(_MPI)
 extern void mpi_init();
 extern void mpi_finalize();
 extern void mpi_get_rank(mdsys_t *sys);
 extern void mpi_get_size(mdsys_t *sys);
+#endif
 
 #ifdef __cplusplus
 }
